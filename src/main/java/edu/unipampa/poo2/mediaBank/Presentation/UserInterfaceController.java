be424a106c8ee3f21385ed41c5c00be232f429a5
Silvio Marques <silvio.mms2016@gmail.com>
2021-04-04 23:22:00 -0300

    fix: fixing UserInterfaceController errors

--- src/main/java/edu/unipampa/poo2/mediaBank/Presentation/UserInterfaceController.java
+++ src/main/java/edu/unipampa/poo2/mediaBank/Presentation/UserInterfaceController.java
@@ -3,13 +3,16 @@ package edu.unipampa.poo2.mediaBank.Presentation;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 
 public class UserInterfaceController implements Initializable {
+    @FXML
     private Label labelTest;
 
+    @FXML
     private void handleClickTest(ActionEvent event) {
         labelTest.setText("Hey, don't click on me, it hurts :(");
     }
