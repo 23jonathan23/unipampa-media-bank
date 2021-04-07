8c8bd97033bc5c88f5e413b25819484059fc64d1
gpmatheus <gp.matheusluis@gmail.com>
2021-04-07 20:12:37 -0300

    ajustment of class names an variablas

--- src/main/java/edu/unipampa/poo2/mediaBank/Business/PhotoHandler.java
+++ src/main/java/edu/unipampa/poo2/mediaBank/Business/PhotoHandler.java
@@ -3,22 +3,22 @@ package edu.unipampa.poo2.mediaBank.Business;
 import edu.unipampa.poo2.mediaBank.Domain.Photo;
 import edu.unipampa.poo2.mediaBank.Domain.FilterMedia;
 import edu.unipampa.poo2.mediaBank.Infra.Repository.DBRepository;
-import edu.unipampa.poo2.mediaBank.Business.utils.MediaHandler;
+import edu.unipampa.poo2.mediaBank.Business.utils.MediaSorter;
 
 import java.io.IOException;
 import java.util.List;
 
-public class PhotoHandler {
+public class PhotoHandler extends MediaHandler {
     
     private List<Photo> photos;
     private DBRepository repository;
-    private MediaHandler mediaHandler;
+    private MediaSorter mediaSorter;
     private FilterMedia filter;
     private boolean sortType = false; //true is Title, false is Date
 
-    public PhotoHandler(DBRepository repository, MediaHandler mediaHandler){
+    public PhotoHandler(DBRepository repository, MediaSorter mediaSorter){
         this.repository = repository;
-        this.mediaHandler = mediaHandler;
+        this.mediaSorter = mediaSorter;
         filter = new FilterMedia();
     }
 
@@ -26,7 +26,7 @@ public class PhotoHandler {
         sortType = type;
     }
 
-    public boolean deletePhoto(int id){
+    public boolean deleteMedia(int id){
         try {
             repository.delete(id);
         } catch (ClassNotFoundException cnf) {
@@ -43,9 +43,9 @@ public class PhotoHandler {
 
         try{
             if (sortType) {
-                photos = mediaHandler.getPhotosByTitle(filter);
+                photos = mediaSorter.getPhotosByTitle(filter);
             } else {
-                photos = mediaHandler.getPhotosByDate(filter);
+                photos = mediaSorter.getPhotosByDate(filter);
             }
         } catch (ClassNotFoundException cnf) {
             return false;
@@ -54,10 +54,10 @@ public class PhotoHandler {
         }
         return true;
     }
-    public List<Photo> getPhotos(){
+    public List<Photo> getPhoto(){
         return photos;
     }
-    public boolean editPhoto(Photo photo) {
+    public boolean edit(Photo photo) {
         try {
             repository.update(photo);
         } catch (ClassNotFoundException cnf) {
