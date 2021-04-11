4c93791bd178630e9977f59918c728fca3a8e5aa
Silvio Marques <silvio.mms2016@gmail.com>
2021-04-11 02:39:35 -0300

    feat: adding some inputs and buttons

--- src/main/java/edu/unipampa/poo2/mediaBank/Presentation/UserInterfaceController.java
+++ src/main/java/edu/unipampa/poo2/mediaBank/Presentation/UserInterfaceController.java
@@ -1,24 +1,45 @@
 package edu.unipampa.poo2.mediaBank.Presentation;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
 
 public class UserInterfaceController implements Initializable {
     @FXML
-    private Label labelTest;
+    private Button addNewMediaButton;
+    @FXML
+    private ChoiceBox<String> orderDropdown;
+    @FXML
+    private ChoiceBox<String> filterDropdown;
+
+    FileChooser fileChooser = new FileChooser();
+
+    private void loadDropdownData() {
+        orderDropdown.getItems().add("A-Z");
+        orderDropdown.getItems().add("Mais recentes");
+        orderDropdown.getItems().add("Mais antigos");
+        orderDropdown.getSelectionModel().select(0);
+
+        filterDropdown.getItems().add("Gênero");
+    }
 
     @FXML
-    private void handleClickTest(ActionEvent event) {
-        labelTest.setText("Hey, don't click on me, it hurts :(");
+    private void handleAddNewMedia(ActionEvent event) {
+        Stage stage = (Stage) addNewMediaButton.getScene().getWindow();
+
+        File file = fileChooser.showOpenDialog(stage);
     }
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // todo
+        loadDropdownData();
     }
 }
