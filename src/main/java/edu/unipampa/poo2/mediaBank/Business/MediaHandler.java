e26e75e5b7abe1b24e16931109a620e5a30b77cc
gpmatheus <gp.matheusluis@gmail.com>
2021-04-11 20:37:19 -0300

    improving its object orientation

--- src/main/java/edu/unipampa/poo2/mediaBank/Business/MediaHandler.java
+++ src/main/java/edu/unipampa/poo2/mediaBank/Business/MediaHandler.java
@@ -1,38 +1,26 @@
 package edu.unipampa.poo2.mediaBank.Business;
 
 import edu.unipampa.poo2.mediaBank.Domain.FilterMedia;
+import edu.unipampa.poo2.mediaBank.Domain.Media;
 import edu.unipampa.poo2.mediaBank.Infra.Repository.DBRepository;
 
 import java.io.IOException;
 
-import edu.unipampa.poo2.mediaBank.Business.utils.MediaSorter;
-
 public abstract class MediaHandler {
     protected DBRepository repository;
-    protected MediaSorter mediaSorter;
     protected static FilterMedia filter = new FilterMedia();
-    protected boolean sortType = false;
 
-    public MediaHandler(DBRepository repository, MediaSorter mediaSorter) {
+    public MediaHandler(DBRepository repository) {
         this.repository = repository;
-        this.mediaSorter = mediaSorter;
-    }
-
-    public void sortByTitle(boolean type) {
-        sortType = type;
-    }
-
-    public boolean removeFromSystem(int id) {
-        try {
-            repository.delete(id);
-        } catch (ClassNotFoundException cnf) {
-            return false;
-        } catch (IOException ioe) {
-            return false;
-        }
-        return true;
     }
     
-    public abstract boolean deleteMedia(int id);
-    public abstract boolean query(String title, String genre);
+    public void deleteMedia(int id) throws ClassNotFoundException, IOException {
+        repository.delete(id);
+    }
+    public void createMedia(Media media) throws ClassNotFoundException, IOException {
+        repository.insert(media);
+    }
+    public void updateMedia(Media media) throws ClassNotFoundException, IOException {
+        repository.update(media);
+    }
 }
