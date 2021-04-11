8c4d53be35def7128e518b06b2067a39829cc460
gpmatheus <gp.matheusluis@gmail.com>
2021-04-11 20:39:01 -0300

    improving its object orientation

--- src/main/java/edu/unipampa/poo2/mediaBank/Business/PhotoHandler.java
+++ src/main/java/edu/unipampa/poo2/mediaBank/Business/PhotoHandler.java
@@ -1,101 +1,30 @@
 package edu.unipampa.poo2.mediaBank.Business;
 
 import edu.unipampa.poo2.mediaBank.Domain.Photo;
+import edu.unipampa.poo2.mediaBank.Domain.SortType;
 import edu.unipampa.poo2.mediaBank.Infra.Repository.DBRepository;
-import edu.unipampa.poo2.mediaBank.Business.utils.MediaSorter;
+import edu.unipampa.poo2.mediaBank.Business.utils.FilterMediaListByType;
 
-import java.io.File;
 import java.io.IOException;
-import java.util.Calendar;
 import java.util.List;
 
 public class PhotoHandler extends MediaHandler {
-    
-    private List<Photo> photos;
 
-    public PhotoHandler(DBRepository repository, MediaSorter mediaSorter){
-        super(repository, mediaSorter);
+    public PhotoHandler(DBRepository repository){
+        super(repository);
     }
 
-    public boolean deleteMedia(int id) {
-        Photo photo = getMedia(id);
-
-        if (photo == null) {
-            return false;
-        }
-
-        File file = new File(photo.getPathFile());
-        file.delete();
-
-        if (!removeFromSystem(id)) {
-            return false;
-        }
-
-        return query(filter.getTitle(), filter.getGenre());
-    }
-
-    public boolean query(String title, String genre){
+    public List<Photo> getPhoto(String title, String genre, SortType sortType) throws ClassNotFoundException, IOException{
         filter.setTitle(title);
         filter.setGenre(genre);
-
-        try{
-            if (sortType) {
-                photos = mediaSorter.getPhotosByTitle(filter);
-            } else {
-                photos = mediaSorter.getPhotosByDate(filter);
-            }
-        } catch (ClassNotFoundException cnf) {
-            return false;
-        } catch (IOException ioe) {
-            return false;
-        }
-        return true;
-    }
-    
-    public List<Photo> getPhotos(){
-        return photos;
-    }
-    public boolean edit(Photo photo) {
-        try {
-            repository.update(photo);
-        } catch (ClassNotFoundException cnf) {
-            return false;
-        } catch (IOException ioe) {
-            return false;
+        var photoList = FilterMediaListByType.extractPhotoList(repository.queryList(filter));
+        
+        if(sortType == SortType.TITLE) {
+           photoList.sort((p1, p2) -> p1.getTitle().compareTo(p2.getTitle()) < 0 ? 1 : 0);
+        } else {
+            photoList.sort((p1, p2) -> p1.getDate().getTimeInMillis() > p2.getDate().getTimeInMillis() ? 1 : 0);
         }
 
-        return query(filter.getTitle(), filter.getGenre());
-    }
-
-    public boolean addPhoto(File file, String title, String description, String photographer, //it has no validation for the photo name
-        List<String> people, String place, Calendar date){
-
-        Photo photo = new Photo(title, description, photographer, people, place, date, file.toPath().toString());
-        try {
-            photo.setId(repository.getNewId());
-        } catch (ClassNotFoundException cnf) {
-            return false;
-        } catch (IOException ioe) {
-            return false;
-        }
-
-        try{
-            repository.insert(photo);
-        } catch(IOException ioe) {
-            return false;
-        } catch(ClassNotFoundException cnf) {
-            return false;
-        }
-
-        return query(filter.getTitle(), filter.getGenre());
-    }
-
-    private Photo getMedia(int id) {
-        for (Photo p : photos) {
-            if (p.getId() == id) {
-                return p;
-            }
-        }
-        return null;
+        return photoList;
     }
 }
