76e7776ca59197a0a7a81abfcbc6db10a656fc41
gpmatheus <gp.matheusluis@gmail.com>
2021-04-11 20:38:55 -0300

    improving its object orientation

--- src/main/java/edu/unipampa/poo2/mediaBank/Business/MovieHandler.java
+++ src/main/java/edu/unipampa/poo2/mediaBank/Business/MovieHandler.java
@@ -1,95 +1,30 @@
 package edu.unipampa.poo2.mediaBank.Business;
 
 import edu.unipampa.poo2.mediaBank.Domain.Movie;
-import edu.unipampa.poo2.mediaBank.Domain.FilterMedia;
+import edu.unipampa.poo2.mediaBank.Domain.SortType;
 import edu.unipampa.poo2.mediaBank.Infra.Repository.DBRepository;
-import edu.unipampa.poo2.mediaBank.Business.utils.MediaSorter;
+import edu.unipampa.poo2.mediaBank.Business.utils.FilterMediaListByType;
 
-import java.io.File;
 import java.io.IOException;
 import java.util.List;
 
-public class MovieHandler extends MediaHandler{
+public class MovieHandler extends MediaHandler {
 
-    List<Movie> movies;
-
-    public MovieHandler(DBRepository repository, MediaSorter mediaSorter){
-        super(repository, mediaSorter);
+    public MovieHandler(DBRepository repository){
+        super(repository);
     }
 
-    public boolean deleteMedia(int id) {
-        Movie movie = getMedia(id);
-
-        if (movie == null) {
-            return false;
-        }
-
-        File file = new File(movie.getPathFile());
-        file.delete();
-
-        try {
-            repository.delete(id);
-        } catch (ClassNotFoundException cnf) {
-            return false;
-        } catch (IOException ioe) {
-            return false;
-        }
-
-        return query(filter.getTitle(), filter.getGenre());
-    }
-
-    public boolean query(String title, String genre){
+    public List<Movie> getMovie(String title, String genre, SortType sortType) throws ClassNotFoundException, IOException{
         filter.setTitle(title);
         filter.setGenre(genre);
-
-        try{
-            if (sortType) {
-                movies = mediaSorter.getMoviesByTitle(filter);
-            } else {
-                movies = mediaSorter.getMoviesByDate(filter);
-            }
-        } catch (ClassNotFoundException cnf) {
-            return false;
-        } catch (IOException ioe) {
-            return false;
-        }
-        return true;
-    }
-
-    public List<Movie> getMovies(){
-        return movies;
-    }
-
-    public boolean edit(Movie movie) {
-        try {
-            repository.update(movie);
-        } catch (ClassNotFoundException cnf) {
-            return false;
-        } catch (IOException ioe) {
-            return false;
+        var movieList = FilterMediaListByType.extractMovieList(repository.queryList(filter));
+        
+        if(sortType == SortType.TITLE) {
+           movieList.sort((p1, p2) -> p1.getTitle().compareTo(p2.getTitle()) < 0 ? 1 : 0);
+        } else {
+            movieList.sort((p1, p2) -> p1.getYear() > p2.getYear() ? 1 : 0);
         }
 
-        return query(filter.getTitle(), filter.getGenre());
-    }
-
-    public boolean addMovie(Movie movie){
-        try{
-            repository.insert(movie);
-        } catch(IOException ioe) {
-            return false;
-        } catch(ClassNotFoundException cnf) {
-            return false;
-        }
-
-        return query(filter.getTitle(), filter.getGenre());
-    }
-
-    private Movie getMedia(int id) {
-        for (Movie p : movies) {
-            if (p.getId() == id) {
-                return p;
-            }
-        }
-        return null;
+        return movieList;
     }
 }
