c72500e1cb7744d63a8867a8d05aa608dca0d1ff
gpmatheus <gp.matheusluis@gmail.com>
2021-04-12 02:13:36 -0300

    simplification of its methods

--- src/main/java/edu/unipampa/poo2/mediaBank/Domain/Movie.java
+++ src/main/java/edu/unipampa/poo2/mediaBank/Domain/Movie.java
@@ -1,12 +1,11 @@
 package edu.unipampa.poo2.mediaBank.Domain;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.time.LocalTime;
 
 public class Movie extends MediaPlayer {
     private String _director;
-    private List <String> _actors = new ArrayList<>();
+    private List <String> _actors;
     private static final long serialVersionUID = 7582232704491799370L;
 
     public Movie(String title, String description, String genre, String language, 
@@ -15,7 +14,7 @@ public class Movie extends MediaPlayer {
         super(title, description, pathFile, genre, language, duration, year);
 
         _director = director;
-        _actors.addAll(actors);
+        _actors = actors;
     }
 
     public void setDirector(String director) {
@@ -26,57 +25,11 @@ public class Movie extends MediaPlayer {
         return _director;
     }
     
-    public void setActor(String actor) throws IllegalArgumentException {
-        boolean found = findActor(actor);
-
-        if (found) {
-            throw new IllegalArgumentException("O ator ja está no filme");
-        }
-
-        _actors.add(actor);
+    public void setActors(List<String> actors) {
+        _actors = actors;
     }
     
-    public void removeActor(String actor) throws IllegalArgumentException {
-        boolean found = findActor(actor);
-
-        if (!found) {
-            throw new IllegalArgumentException("O ator não está no filme");
-        }
-
-        String actualActor = _actors.get(getIndex(actor));
-
-        _actors.remove(actualActor);
-    }
-    
-    public String actorsToString() {
-        if (_actors.size() == 0) {
-            return null;
-        }
-
-        String allActors = "";
-
-        for (String s : _actors) {
-            allActors += s + ";";
-        }
-
-        return allActors;
-    }
-
-    private int getIndex(String actor) {
-        for (String s : _actors){
-            if (s.equalsIgnoreCase(actor)) {
-                return _actors.indexOf(s);
-            }
-        }
-        return 0;
-    }
-
-    private boolean findActor(String actor) {
-        for (String s : _actors){
-            if (s.equalsIgnoreCase(actor)) {
-                return true;
-            }
-        }
-        return false;
+    public List<String> getActors() {
+        return _actors;
     }
 }
--- src/main/java/edu/unipampa/poo2/mediaBank/Domain/Song.java
+++ src/main/java/edu/unipampa/poo2/mediaBank/Domain/Song.java
@@ -1,12 +1,11 @@
 package edu.unipampa.poo2.mediaBank.Domain;
 
 import java.util.List;
-import java.util.ArrayList;
 import java.time.LocalTime;
 
 public class Song extends MediaPlayer {
-    private List<String> _authors = new ArrayList<>();
-    private List<String> _interpreters = new ArrayList<>();
+    private List<String> _authors;
+    private List<String> _interpreters;
     private static final long serialVersionUID = 7884718496500956708L;
 
     public Song(String title, String description, String genre,
@@ -15,114 +14,23 @@ public class Song extends MediaPlayer {
         
         super(title, description, pathFile, genre, language, duration, year);
         
-        _authors.addAll(authors);
-        _interpreters.addAll(interpreters);
+        _authors = authors;
+        _interpreters = interpreters;
     }
 
-    public void setAuthor(String author) throws IllegalArgumentException {
-        boolean authorFound = findAuthor(author);
-
-        if (authorFound) {
-            throw new IllegalArgumentException("Já é autor da música");
-        }
-
-        _authors.add(author);
+    public void setAuthors(List<String> authors) {
+        _authors = authors;
     }
 
-    public void removeAuthor(String author) throws IllegalArgumentException {
-        boolean authorFound = findAuthor(author);
-
-        if (!authorFound) {
-            throw new IllegalArgumentException("Não é autor da música");
-        }
-
-        String actualAuthor = _authors.get(getIndexAuthor(author));
-
-        _authors.remove(actualAuthor);
+    public List<String> getAuthors() {
+        return _authors;
     }
 
-    private int getIndexAuthor(String author) {
-        for (String s : _authors) {
-            if (s.equalsIgnoreCase(author)) {
-                return _authors.indexOf(s);
-            }
-        }
-        return 0;
+    public void setInterpreters(List<String> interpreters) {
+        _interpreters = interpreters;
     }
 
-    public String authorsToString() {
-        if (_authors.size() == 0) {
-            return null;
-        }
-
-        String allAuthors = "";
-
-        for (String s : _authors) {
-            allAuthors += s + ";";
-        }
-
-        return allAuthors;
+    public List<String> getInterpreters() {
+        return _interpreters;
     }
-
-    public void setInterpreter(String interpreter) throws IllegalArgumentException {
-        boolean interpreterFound = findInterpreter(interpreter);
-
-        if (interpreterFound) {
-            throw new IllegalArgumentException("Já é intérprete da música");
-        }
-
-        _interpreters.add(interpreter);
-    }
-
-    public void removeInterpreter(String interpreter) throws IllegalArgumentException {
-        boolean interpreterFound = findInterpreter(interpreter);
-
-        if (!interpreterFound){
-            throw new IllegalArgumentException("Não é intérprete da música");
-        }
-
-        String actualInterpreter = _interpreters.get(getIndexInterpreter(interpreter));
-        _interpreters.remove(actualInterpreter);
-    }
-
-    public String interpretersToString() {
-        if (_interpreters.size() == 0){
-            return null;
-        }
-
-        String allInterpreters = "";
-
-        for (String s : _interpreters) {
-            allInterpreters += s + ";";
-        }
-
-        return allInterpreters;
-    }
-
-    private int getIndexInterpreter(String interpreter) {
-        for (String s : _interpreters) {
-            if (s.equalsIgnoreCase(interpreter)) {
-                return _interpreters.indexOf(s);
-            }
-        }
-        return 0;
-    }
-
-    private boolean findInterpreter(String interpreter) {
-        for (String s : _interpreters){
-            if (s.equalsIgnoreCase(interpreter)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private boolean findAuthor(String author) {
-        for (String s : _authors){
-            if (s.equalsIgnoreCase(author)) {
-                return true;
-            }
-        }
-        return false;
-    } 
 }
